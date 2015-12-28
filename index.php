a09d808b9aac4f8d3d9e01ebef76c329e2793626
CBA_Santo <santo@cybridge.jp>
2015-12-28 10:43:42 +0900

    commit

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 	<!-- Bootstrap CSS -->
 	<link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
 	<link href="vendor/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+	<link href="vendor/css/style.css" rel="stylesheet">
 
 	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -16,160 +17,6 @@
 			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 			<![endif]-->
-			<style>
-				.thumbnail {
-					height: 300px;
-					overflow: auto;
-					display: table-cell;
-					vertical-align: middle;
-				}
-				.thumbnail img{
-					max-height: 290px;
-				}
-
-
-				/* sdfasdf */
-
-				.full {
-				    width: 100%;	
-				}
-				.gap {
-					height: 30px;
-					width: 100%;
-					clear: both;
-					display: block;
-				}
-				.footer {
-					background: #EDEFF1;
-					height: auto;
-					padding-bottom: 30px;
-					position: relative;
-					width: 100%;
-					border-bottom: 1px solid #CCCCCC;
-					border-top: 1px solid #DDDDDD;
-				}
-				.footer p {
-					margin: 0;
-				}
-				.footer img {
-					max-width: 100%;
-				}
-				.footer h3 {
-					border-bottom: 1px solid #BAC1C8;
-					color: #54697E;
-					font-size: 18px;
-					font-weight: 600;
-					line-height: 27px;
-					padding: 40px 0 10px;
-					text-transform: uppercase;
-				}
-				.footer ul {
-					font-size: 13px;
-					list-style-type: none;
-					margin-left: 0;
-					padding-left: 0;
-					margin-top: 15px;
-					color: #7F8C8D;
-				}
-				.footer ul li a {
-					padding: 0 0 5px 0;
-					display: block;
-				}
-				.footer a {
-					color: #78828D
-				}
-				.supportLi h4 {
-					font-size: 20px;
-					font-weight: lighter;
-					line-height: normal;
-					margin-bottom: 0 !important;
-					padding-bottom: 0;
-				}
-				.newsletter-box input#appendedInputButton {
-					background: #FFFFFF;
-					display: inline-block;
-					float: left;
-					height: 30px;
-					clear: both;
-					width: 100%;
-				}
-				.newsletter-box .btn {
-					border: medium none;
-					-webkit-border-radius: 3px;
-					-moz-border-radius: 3px;
-					-o-border-radius: 3px;
-					-ms-border-radius: 3px;
-					border-radius: 3px;
-					display: inline-block;
-					height: 40px;
-					padding: 0;
-					width: 100%;
-					color: #fff;
-				}
-				.newsletter-box {
-					overflow: hidden;
-				}
-				.bg-gray {
-					background-image: -moz-linear-gradient(center bottom, #BBBBBB 0%, #F0F0F0 100%);
-					box-shadow: 0 1px 0 #B4B3B3;
-				}
-				.social li {
-					background: none repeat scroll 0 0 #B5B5B5;
-					border: 2px solid #B5B5B5;
-					-webkit-border-radius: 50%;
-					-moz-border-radius: 50%;
-					-o-border-radius: 50%;
-					-ms-border-radius: 50%;
-					border-radius: 50%;
-					float: left;
-					height: 36px;
-					line-height: 36px;
-					margin: 0 8px 0 0;
-					padding: 0;
-					text-align: center;
-					width: 36px;
-					transition: all 0.5s ease 0s;
-					-moz-transition: all 0.5s ease 0s;
-					-webkit-transition: all 0.5s ease 0s;
-					-ms-transition: all 0.5s ease 0s;
-					-o-transition: all 0.5s ease 0s;
-				}
-				.social li:hover {
-					transform: scale(1.15) rotate(360deg);
-					-webkit-transform: scale(1.1) rotate(360deg);
-					-moz-transform: scale(1.1) rotate(360deg);
-					-ms-transform: scale(1.1) rotate(360deg);
-					-o-transform: scale(1.1) rotate(360deg);
-				}
-				.social li a {
-					color: #EDEFF1;
-				}
-				.social li:hover {
-					border: 2px solid #2c3e50;
-					background: #2c3e50;
-				}
-				.social li a i {
-					font-size: 16px;
-					margin: 0 0 0 5px;
-					color: #EDEFF1 !important;
-				}
-				.footer-bottom {
-					background: #E3E3E3;
-					border-top: 1px solid #DDDDDD;
-					padding-top: 10px;
-					padding-bottom: 10px;
-				}
-				.footer-bottom p.pull-left {
-					padding-top: 6px;
-				}
-				.payments {
-					font-size: 1.5em;	
-				}
-				.fa-facebook-f:after, .fa-facebook:after {
-					content: "" !important;
-				}
-			</style>
-			
 		</head>
 		<body>
 			<?php
@@ -229,7 +76,14 @@
 					"link_img"     => "store/00_images_preview/2015-12-25 13_11_52-Alliance - A Responsive Bootstrap 3 Admin Dashboard Template.png",
 					"price"        => "23$",
 				],
-				
+
+				[
+					"name_theme"   => "Smartstart",
+					"link_zip"     => "store/smartstart/smartstart.zip",
+					"link_preview" => "store/smartstart",
+					"link_img"     => "store/00_images_preview/2015-12-28 08_40_41.png",
+					"price"        => "23$",
+				],
 
 			];
 
